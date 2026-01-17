a43fa5ce1ce85aed143caf3493f3de19f5291888
DHIBIN VIKASH K P <83573561+DHIBIN-VIKASH@users.noreply.github.com>
2026-01-18 02:56:43 +0530

    Update catch-all to Express 5 named parameter syntax

--- server.cjs
+++ server.cjs
@@ -116,7 +116,7 @@ app.post('/api/update', async (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('(.*)', (req, res) => {
+app.get('/:path*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
